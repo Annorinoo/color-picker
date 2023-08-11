714df571c880292f077616ed05896d78cc9e1c4a
Annorinoo <annjenelr@gmail.com>
2023-08-10 18:01:38 -0700

    Finished picking colros and adding an outline to selected object

--- Unit3.ColorPicker.Starter-main/src/App.jsx
+++ Unit3.ColorPicker.Starter-main/src/App.jsx
@@ -1,22 +1,30 @@
-import { useState } from "react";
+import React, { useState } from 'react';
+import './index.css'; // Make sure to import your CSS file if not already done
 
-const Color = (props) => {
-  return <div className={props.color}></div>
-}
+const Color = ({ color, selectedColor, setSelectedColor }) => {
+  const isSelected = selectedColor === color;
+
+  return (
+    <div
+      className={`${color} ${isSelected ? 'selected' : ''}`}
+      onClick={() => setSelectedColor(color)}
+    ></div>
+  );
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState('');
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
-        <div className="blue">blue</div>
-        <div className="yellow">yellow</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">
-        <Color color="red"/>
-        <Color color="blue"/>
-        <Color color="yellow"/>
+        <div id="colors-list">
+        <Color color="red" selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <Color color="blue" selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <Color color="green" selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
       </div>
     </div>
   );
